bd14f198714c62ef7a0d8e3406854bb362f3faac
selontek <153787657+selontek@users.noreply.github.com>
2024-02-03 14:01:24 +0700

    Create konekasid.php

---
+++ konekasid.php
@@ -0,0 +1,24 @@
+<?php
+            include_once("koneksi.php");
+            $sql_dan = "SELECT * FROM dana";
+            $result_dan = mysqli_query($con,$sql_dan);
+            $dan = mysqli_num_rows($result_dan)
+            ?>
+<?php
+            include_once("koneksi.php");
+            $sql_bel = "SELECT * FROM belanja";
+            $result_bel = mysqli_query($con,$sql_bel);
+            $bel = mysqli_num_rows($result_bel)
+            ?>
+<?php
+            include_once("koneksi.php");
+            $sql_ran = "SELECT * FROM rancangan";
+            $result_ran = mysqli_query($con,$sql_ran);
+            $ran = mysqli_num_rows($result_ran)
+            ?>
+<?php
+            include_once("koneksi.php");
+            $sql_dan = "SELECT * FROM dana";
+            $result_dan = mysqli_query($con,$sql_dan);
+            $dan = mysqli_num_rows($result_dan)
+            ?>
